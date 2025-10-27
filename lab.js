2a83473c7d31e809ca8a48a368d91e31503c9155
viktorradu <5158155+viktorradu@users.noreply.github.com>
2025-10-27 15:42:59 -0400

    irrelevant IDs config removed

--- lab.js
+++ lab.js
@@ -1,8 +1,8 @@
 var pbilab = {
-	tenantId: "61f85cf3-dc1a-4c9f-b39f-45caf3aef59d",
-	clientId: "1de9c89a-f46a-479d-a84c-b93cb58fb1fa",
-	reportId: "ac9f6b37-f0d6-4fd5-b136-f29edcf8cac5",
-	userUpn: "admin@MngEnvMCAP118086.onmicrosoft.com",
+	tenantId: "...",
+	clientId: "...",
+	reportId: "...",
+	userUpn: "...",
 	
 	pipeConfig: (config) =>{
 		
